6b92c0b8c027f91cd8bed898c504424521364d50
LeeGuSeul <rntmf1247@naver.com>
2020-09-02 06:30:43 +0900

    Department registration new page

---
+++ pop_department_regist.php
@@ -0,0 +1,17 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/head.php"; ?>
+     <!-- // -->
+    <div class="popup depart_regist">
+        <p class="tit">학과등록</p>
+
+        <div class="gray_bg">
+            <label for="">학과명 <input type="text" placeholder="학과명 입력" /></label>
+            <label for="">줄임말 <input type="text" placeholder="학과명 줄임말 입력" /></label>
+        </div>
+
+        <div class="btn_area">
+            <a href="" class="bbtn mint">저장</a>
+            <a href="" class="bbtn darkGray">취소</a>
+        </div>
+    </div>
+    <!-- // -->
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/footer.php"; ?>
