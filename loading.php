03fea19c2b7effbb4220632fc5038945f64b1efa
LeeGuSeul <rntmf1247@naver.com>
2020-09-03 21:16:46 +0900

    Loading Page Update

---
+++ loading.php
@@ -0,0 +1,7 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/head.php"; ?>
+     <!-- // -->
+    <div class="popup loading">
+        <p>Loading ...</p>
+    </div>
+    <!-- // -->
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/footer.php"; ?>
