d123dc1d872963c884ad96e705ed347d009db293
LeeGuSeul <rntmf1247@naver.com>
2020-10-03 22:22:20 +0900

    Web::Board View

---
+++ popup_board_view.php
@@ -0,0 +1,18 @@
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/head.php"; ?>
+    <div class="popup board">
+        <div class="view_area">
+            <p class="noti_tit">청양먹거리직매장 유성점 오픈 및 구매후기 이벤트</p>
+            <ul class="noti_info">
+                <li>작성일 <span>2020.09.29</span></li>
+                <li>조회수 <span>151</span></li>
+            </ul>
+
+            <!-- 글내용 -->
+            <div class="noti_view">맛있었겠네</div>
+        </div>
+
+        <div class="btn_area">
+            <div class="darkGray">목록보기</div>
+        </div>
+    </div>
+<?php include $_SERVER['DOCUMENT_ROOT']."/global/file/footer.php"; ?>
