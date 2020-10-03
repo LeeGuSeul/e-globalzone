d76eccfda74a665454ccd6ccfd263b3e5478088d
LeeGuSeul <rntmf1247@naver.com>
2020-10-03 19:16:16 +0900

    Web::Board Write File

--- popup_board_write.php
+++ popup_board_write.php
@@ -9,10 +9,25 @@
             <p>내용</p>
             <textarea name="" id="" placeholder="제목을 입력해주세요."></textarea>
         </div>
-        
+        <div class="board_file_row">
+            <div class="tit">첨부파일</div>
+            <div class="file_area">
+                <div class="filebox">
+                    <label for="ex_file">사진 업로드</label>
+                    <input type="file" id="ex_file" />
+                </div>
+                <ul>
+                    <li><span>1.png</span> <div class="del_btn"><img src="/global/img/file_del_btn.gif" alt="파일 삭제"></div></li>
+                    <li><span>1.png</span> <div class="del_btn"><img src="/global/img/file_del_btn.gif" alt="파일 삭제"></div></li>
+                    <li><span>1.png</span> <div class="del_btn"><img src="/global/img/file_del_btn.gif" alt="파일 삭제"></div></li>
+                    <li><span>1.png</span> <div class="del_btn"><img src="/global/img/file_del_btn.gif" alt="파일 삭제"></div></li>
+                    <li><span>1.png</span> <div class="del_btn"><img src="/global/img/file_del_btn.gif" alt="파일 삭제"></div></li>
+                </ul>
+            </div>
+        </div>
 
         <div class="btn_area">
-            <div class="mint">글 저장</div>
+            <div class="darkGray">글 저장</div>
         </div>
     </div>
 <?php include $_SERVER['DOCUMENT_ROOT']."/global/file/footer.php"; ?>
